1a5e266d3ed32bee7cea2e9a4696fb9403711a97
yuriap <yuriap@gmail.com>
2017-12-04 12:52:08 +0200

    Version 2.4.2

--- findq.sql
+++ findq.sql
@@ -1,3 +1,5 @@
+define hver=1
+
 alter session set nls_date_format='YYYY-MON-DD HH24:mi:ss';
 column INST_ID format a7
 column INST_NAME format a20
@@ -17,19 +19,19 @@ prompt Mode is &fscope
 column INST_ID format 9999
 rem prompt Fast search 
 select cmd, LAST_ACTIVE_TIME, inst_id,sqltext from (
-select /*+qb_name(findq)*/ '@getplan '||sql_id cmd, to_char(LAST_ACTIVE_TIME,'yyyy-mm-dd hh24:mi:ss') LAST_ACTIVE_TIME, inst_id,replace(replace(sql_text,chr(13),' '),chr(13),' ') sqltext 
+select /*+qb_name(findq)*/ '@getplan'||decode(&hver.,1,'h',null)||' '||sql_id cmd, to_char(LAST_ACTIVE_TIME,'yyyy-mm-dd hh24:mi:ss') LAST_ACTIVE_TIME, inst_id,replace(replace(sql_text,chr(13),' '),chr(13),' ') sqltext 
   from gv$sqlstats
  where upper(sql_fulltext) like upper('&1') 
    and sql_text not like '%qb_name(findq)%' 
    and sql_text not like 'explain plan%' 
    and '&fscope'='fast'
 union all
-select /*+qb_name(findq)*/ '@getplanawr '||sql_id,(select to_char(min(snap_id)) ||' '|| to_char(max(snap_id)) ma_snap_id from dba_hist_sqlstat s where s.sql_id=t.sql_id) LAST_ACTIVE_TIME,null,null --cast(substr(sql_text,1,1000) as varchar2(1000))  
+select /*+qb_name(findq)*/ '@getplanawr'||decode(&hver.,1,'h',null)||' '||sql_id,(select to_char(min(snap_id)) ||' '|| to_char(max(snap_id)) ma_snap_id from dba_hist_sqlstat s where s.sql_id=t.sql_id) LAST_ACTIVE_TIME,null,null --cast(substr(sql_text,1,1000) as varchar2(1000))  
   from dba_hist_sqltext t
  where upper(sql_text) like upper('&1') 
    and '&fscope'='awr'
 union all
-select /*+qb_name(findq)*/ '@getplan '||sql_id, to_char(LAST_ACTIVE_TIME,'yyyy-mm-dd hh24:mi:ss') LAST_ACTIVE_TIME, inst_id,replace(replace(sql_text,chr(13),' '),chr(13),' ')  
+select /*+qb_name(findq)*/ '@getplan'||decode(&hver.,1,'h',null)||' '||sql_id, to_char(LAST_ACTIVE_TIME,'yyyy-mm-dd hh24:mi:ss') LAST_ACTIVE_TIME, inst_id,replace(replace(sql_text,chr(13),' '),chr(13),' ')  
   from gv$sql 
  where upper(sql_fulltext) like upper('&1') 
    and sql_text not like '%qb_name(findq)%' 
